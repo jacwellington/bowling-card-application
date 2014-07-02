8d6f889306db8803e50edd2fd8930ba39c7deb54
Jacob Wellingtn <jacob.m.wellington@gmail.com>
2014-07-02 16:12:25 -0700

    Refactored create into multiple methods to reduce complexity.

--- app/controllers/bowling_games_controller.rb
+++ app/controllers/bowling_games_controller.rb
@@ -27,28 +27,48 @@ class BowlingGamesController < ApplicationController
     @bowling_game = BowlingGame.new(bowling_game_params)
     @bowling_game.user_id = current_user.id
     if @bowling_game.finished?
-      if @bowling_game.valid?
-        @bowling_game.score_game
-        @bowling_game.save
-        redirect_to bowling_games_path
-      else
-        error = nil
-        @bowling_game.frames.each do |frame|
-          if frame.errors.count > 0
-            error = "Frame number #{frame.number} - #{frame.errors.full_messages.first}"
-          end
-        end
-        error = "Errors: " + @bowling_game.errors.full_messages.first if !error
-        flash[:error] = error
-        render :new
-      end
+      try_score_and_save_game
     else
-      flash[:error] = "You did not complete the bowling game." 
-      render :new
+      render_new_with_error "You did not complete the bowling game."
     end
   end
 
   private 
+
+  # Attempts to save @bowling_game. Renders an error if not valid.
+  def try_score_and_save_game
+    if @bowling_game.valid?
+      @bowling_game.score_game
+      @bowling_game.save
+      redirect_to bowling_games_path
+    else
+      render_new_with_error
+    end
+  end
+
+  # Renders the new template with an error message.
+  #
+  # @params error [String] An optional error message to render.
+  #   If error is not passed in (or nil), then find the error on the @bowling_game object.
+  def render_new_with_error(error = nil)
+    unless error
+      error = find_error_on_bowling_game
+    end
+    flash[:error] = error
+    render :new
+  end
+
+  # Finds the error on the bowling game.
+  def find_error_on_bowling_game
+    @bowling_game.frames.each do |frame|
+      if frame.errors.count > 0
+        error = "Frame number #{frame.number} - #{frame.errors.full_messages.first}"
+      end
+    end
+    error = "Errors: " + @bowling_game.errors.full_messages.first unless error
+    return error
+  end
+
   def bowling_game_params
     params.require(:bowling_game).permit(:user_id, frames_attributes: [:id, :first_throw, :second_throw, :third_throw, :number], comments_attributes: [:id, :body])
   end
